4714ba0949fb52a0eec565b73610950df061860a
singhmayank9846-prog <singhmayank9846@gmail.com>
2026-01-03 08:26:38 +0530

    Add files via upload
    ## Python Mini Assignment
    
    This repository contains my Python practice code.
    
    ### Concepts Covered:
    - Comments
    - Variables
    - Print statements
    
    ### Output:
    Program runs successfully without errors.

---
+++ 2-Comment & Variables.py
@@ -0,0 +1,13 @@
+# Need To Know
+print("Hello World How Are You")
+# Again We Need To Know About The Facts Very Well
+print("Subject Matters")
+# VARIABLES
+name = "Mayank"
+company = "GOOGLE"
+salary = 150000
+DOJ = "2026/03/12"
+print("Myself",name)
+print("currently working in",company)
+print("My Salary Is",salary)
+print("My Date Of Joining Is",DOJ)
